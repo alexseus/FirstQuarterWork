edb97236d153e2e6bf7d4f1c62d506eb364db81e
alex <git@alexsei.ru>
2023-02-16 11:38:04 +0300

    program.cs complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Написать программу, которая из имеющегося массива строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] CreateStringArray(int arrayLenght)
+{
+    string[] stringArray = new string[arrayLenght];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"Введите {i} строку массива: ");
+        stringArray[i] = Console.ReadLine()!;
+    }
+    return stringArray;
+}
+
+void ShowFilteredArray(string[] array, int filterLenght)
+{
+    Console.Write($"Строки в массиве длиной <= {filterLenght} символов: [");
+    int comma = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= filterLenght)
+        {
+            if (comma == 1 && i < array.Length) Console.Write(", ");
+            Console.Write($"\"{array[i]}\"");
+            comma = 1;
+        }
+
+    }
+    Console.Write("]");
+}
+
+
+string[] srtingArray = CreateStringArray(5);    // формируем запрос массива строк, указывая размер массива
+ShowFilteredArray(srtingArray, 3);              // выводим используя фильтр-длину строки
